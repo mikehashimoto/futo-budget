6b8857602e7e05b99e9d03ef8464c23f1aa345f3
Michael Hashimoto <mike.hashi@liferay.com>
2023-09-23 15:15:35 -0700

    FUTO-1 Add TailwindCSS to the frontend

--- webpack.config.js
+++ webpack.config.js
@@ -2,13 +2,26 @@ const path = require('path');
 
 module.exports = {
 	devtool: "source-map",
-	entry: path.resolve(__dirname, 'src/main/js/index.js'),
+	entry: path.resolve(__dirname, 'src/main/frontend/main.js'),
 	module: {
 		rules: [
 			{
 				test: /\.js$/,
 				enforce: "pre",
 				use: ["source-map-loader"],
+			},
+			{
+				test: /\.js$/i,
+				use: {
+					loader: 'babel-loader',
+					options: {
+						presets: ['@babel/preset-env'],
+					},
+				},
+			},
+			{
+				test: /\.css$/i,
+				use: ['style-loader', 'css-loader', 'postcss-loader'],
 			},
 		],
 	},
