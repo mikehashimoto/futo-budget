110468b165eafc355ba93509e82602f11cb85a98
Michael Hashimoto <mike.hashi@liferay.com>
2023-09-23 13:34:38 -0700

    FUTO-1 Add application class for spring start

---
+++ src/main/java/com/mikehashimoto/futo/FutoApplication.java
@@ -0,0 +1,13 @@
+package com.mikehashimoto.futo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FutoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FutoApplication.class, args);
+	}
+
+}
