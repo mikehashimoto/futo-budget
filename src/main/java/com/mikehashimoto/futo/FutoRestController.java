b607722fb20bd2afb9e41d50827ff1b1e6f7fa87
Michael Hashimoto <mike.hashi@liferay.com>
2023-09-23 14:16:44 -0700

    FUTO-1 Add one example of replacing data in a thymeleaf template

--- src/main/java/com/mikehashimoto/futo/FutoRestController.java
+++ src/main/java/com/mikehashimoto/futo/FutoRestController.java
@@ -1,13 +1,16 @@
 package com.mikehashimoto.futo;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class ThymeleafRestController {
+public class FutoRestController {
 
 	@RequestMapping(value = "/")
-	public String index() {
+	public String index(Model model) {
+		model.addAttribute("message", "Hello, World!");
+
 		return "index";
 	}
 
