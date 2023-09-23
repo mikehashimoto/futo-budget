27b56fa759df36c476897bfba77dc9d7edbe4a08
Michael Hashimoto <mike.hashi@liferay.com>
2023-09-23 14:55:58 -0700

    FUTO-1 Add a source map loader

--- src/main/resources/static/js/bundle.js
+++ src/main/resources/static/js/bundle.js
@@ -1,1 +1,2 @@
 console.log("Hello World!");
+//# sourceMappingURL=bundle.js.map
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,17 @@
 const path = require('path');
 
 module.exports = {
+	devtool: "source-map",
 	entry: path.resolve(__dirname, 'src/main/js/index.js'),
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				enforce: "pre",
+				use: ["source-map-loader"],
+			},
+		],
+	},
 	output: {
 		path: path.resolve(__dirname, 'src/main/resources/static/js'),
 		filename: 'bundle.js',
