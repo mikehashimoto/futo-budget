0758336bb4041d65bee8640be473a4c29e8bae5a
Michael Hashimoto <mike.hashi@liferay.com>
2023-09-23 18:12:46 -0700

    FUTO-1 Show an example of how to create an HTML snippet

--- src/main/java/com/mikehashimoto/futo/FutoRestController.java
+++ src/main/java/com/mikehashimoto/futo/FutoRestController.java
@@ -4,6 +4,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 @Controller
 public class FutoRestController {
 
@@ -15,8 +19,19 @@ public class FutoRestController {
 	}
 
 	@RequestMapping(value = "/clicked")
-	public String clicked() {
+	public String clicked(Model model) {
+		int index = count % 3;
+
+		count++;
+
+		model.addAttribute("message", messages.get(index));
+
 		return "component/clicked";
 	}
+
+	private int count = 0;
+
+	private List<String> messages = Arrays.asList(
+		"One", "Two", "Three");
 
 }
